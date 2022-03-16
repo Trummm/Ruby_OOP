699e2f026b8625ce9a322ae0ea432ca0c543abe9
RS-VinhDT <80232936+rs-vinhdt@users.noreply.github.com>
2022-03-16 12:04:45 +0700

    Update ex1.rb

--- ex1.rb
+++ ex1.rb
@@ -26,3 +26,31 @@ puts bird.name
 puts bird.age_day
 puts bird.age_week
 puts bird.action
+
+
+
+# VinhDT
+# class FluffyAnimal
+#   attr_accessor :name, :age_day
+#   attr_writer :age_week, :action
+
+#   def initialize(name, age_day)
+#     @name = name
+#     @age_day = age_day
+#   end
+
+#   def age_week
+#     age_day / 7
+#   end
+
+#   def action
+#     'wriggle cutely'
+#   end
+# end
+
+# bird = FluffyAnimal.new('Chim sao', 7)
+
+# puts bird.name
+# puts bird.age_day
+# puts bird.age_week
+# puts bird.action
